b0c5993050755af1c890bff866ca7a1a8dc6dc0f
Aaradhaya Singh <aaradhayasingh811@gmail.com>
2025-09-03 21:27:54 +0530

    added cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const limiter = rateLimit({
 
 // CORS
 app.use(cors({
-  origin: [process.env.FRONTEND_URL , "http://localhost:5173"],
+  origin: [process.env.FRONTEND_URL , process.env.ADMIN_FRONTEND_URL],
   credentials: true,
 
 }));
